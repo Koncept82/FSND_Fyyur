a184cf36fbaab3d6038291343602c35bed034862
Alf Graham <alfgraham@mac.com>
2020-08-27 11:54:50 +0100

    Imports added for migrate, ARRAY and sys. Ran first migrate and upgrade

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.dialects.postgresql import ARRAY
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+import sys
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,8 +23,10 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://alfgraham@localhost:5432/fyyur'
 
 #----------------------------------------------------------------------------#
 # Models.
